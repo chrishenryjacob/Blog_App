a217066d0f0863aecdf75cc23435074b29ca85c2
Christopher <christopherhenryjacob@gmail.com>
2022-11-06 12:53:21 +0530

    component logc change added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NzMessageService } from 'ng-zorro-antd/message';
-import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +6,5 @@ import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  blogContent !: string;
-
-  constructor(private msg: NzMessageService) { }
-
-  handleChange(info: NzUploadChangeParam): void {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === 'done') {
-      this.msg.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      this.msg.error(`${info.file.name} file upload failed.`);
-    }
-  }
 
 }
--- src/app/blog-form/blog-form.component.ts
+++ src/app/blog-form/blog-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-blog-form',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogFormComponent implements OnInit {
 
-  constructor() { }
+  blogContent !: string;
+
+  constructor(private msg: NzMessageService) { }
 
   ngOnInit(): void {
   }
 
+
+  handleChange(info: NzUploadChangeParam): void {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      this.msg.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      this.msg.error(`${info.file.name} file upload failed.`);
+    }
+  }
 }
