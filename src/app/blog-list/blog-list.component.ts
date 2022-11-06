1c73f422bd76cadbbebee4d0d5993e4add1848ff
Christopher <christopherhenryjacob@gmail.com>
2022-11-06 15:42:36 +0530

    added nested commenting functionality

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { IBlog } from './blog.model';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-blog-list',
@@ -9,6 +10,12 @@ import { IBlog } from './blog.model';
 export class BlogListComponent implements OnInit {
 
   @Input() dataSource: IBlog[] = [];
+
+  selectedId: number = 0;
+  avatar = 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png';
+  userComment!: string;
+  submitting = false;
+  file!: NzUploadFile;
 
   constructor() { }
 
@@ -21,8 +28,23 @@ export class BlogListComponent implements OnInit {
     this.updateBlogDetails()
   }
 
-  addComments(id: number) {
+  addComments(data: IBlog): void {
+    this.submitting = true;
 
+    data.children.push({
+      id: this.selectedId + 100,
+      author: 'chris',
+      avatar: this.avatar,
+      content: this.userComment,
+      likes: 0,
+      img: this.file,
+      children: []
+    })
+
+    this.updateBlogDetails();
+    this.selectedId = 0;
+    this.userComment = '';
+    this.submitting = false;
   }
 
   updateBlogDetails() {
